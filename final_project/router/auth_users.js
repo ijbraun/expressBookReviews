3d3a1a62686d08145748cf77ff5aef4fb02a063a
ijbraun <9153212+ijbraun@users.noreply.github.com>
2025-04-23 15:07:36 -0400

    added functionality

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,21 +10,49 @@ const isValid = (username)=>{ //returns boolean
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+      if (authenticatedUser(username, password)) {
+          let accessToken = jwt.sign({
+              data: password
+          }, 'access', { expiresIn: 60 * 60 });
+  
+          req.session.authorization = {
+              accessToken, username
+          }
+          return res.status(200).send("User successfully logged in");
+      } else {
+          return res.status(208).json({ message: "Invalid Login. Check username and password" });
+      }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    books[isbn].reviews[req.session.authorization.username] = req.body.review;
+
+    return res.send(JSON.stringify({message:"Review Added", book: books[isbn]}));
 });
 
+regd_users.delete("/auth/review/:isbn",(req,res)=>{
+    const isbn = req.params.isbn;
+    delete books[isbn].reviews[req.session.authorization.username];
+    return res.send(JSON.stringify({message:"Review Deleted", book: books[isbn]}));
+})
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
